3e132dce378a3860556b6f045baf72ee85f297f2
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-07-06 12:39:35 +0200

    feat: include main to parse configuration

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,9 @@ ARG TARGETARCH
 RUN apk add --no-cache --upgrade && apk add --no-cache tini curl bind-tools
 
 COPY bin/configurator-${TARGETOS}-${TARGETARCH} /
+COPY configurator/testdata/input-test.yaml /
 
 RUN mv /configurator-${TARGETOS}-${TARGETARCH} /configurator && \
     chmod 755 /configurator
 
-ENTRYPOINT ["/sbin/tini", "--", "/configurator"]
+ENTRYPOINT ["/sbin/tini", "--", "/configurator", "-input", "/input-test.yaml", "-output", "/etc/prometheus/config/config.yaml"]
--- cmd/configurator/configurator.go
+++ cmd/configurator/configurator.go
@@ -1,23 +1,78 @@
 package main
 
 import (
+	"flag"
+	"io"
+	"io/ioutil"
 	"os"
+
+	"newrelic-prometheus/configurator"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	logger := log.StandardLogger()
-	fakeConfig := []byte(`
-# mock config already converted
-remote_write:
-- name: test
-  url: https://fake-endpoint:8000
-`)
-	if err := os.WriteFile("./etc/prometheus/config/config.yaml", fakeConfig, 0444); err != nil {
-		os.Exit(1)
-		logger.Fatalf("fail to create config file: %s", err)
-	}
+const (
+	inputErrCode = iota + 1
+	outputErrCode
+	parseErrCode
+)
 
-	logger.Printf("config created")
+var logger = log.StandardLogger()
+
+func readInput(inputPath string) ([]byte, error) {
+	var reader io.Reader
+	if inputPath == "" {
+		reader = os.Stdin
+	} else {
+		fileReader, err := os.Open(inputPath)
+		if err != nil {
+			return nil, err
+		}
+		defer closeLoggingErr(fileReader)
+		reader = fileReader
+	}
+	return ioutil.ReadAll(reader)
+}
+
+func writeOutput(outputPath string, output []byte) error {
+	var writer io.Writer
+	if outputPath == "" {
+		writer = os.Stdout
+	} else {
+		fileWriter, err := os.Create(outputPath)
+		if err != nil {
+			return err
+		}
+		defer closeLoggingErr(fileWriter)
+		writer = fileWriter
+	}
+	_, err := writer.Write(output)
+	return err
+}
+
+func closeLoggingErr(f *os.File) {
+	if err := f.Close(); err != nil {
+		logger.Errorf("Fail closing file: %s", err)
+	}
+}
+
+func main() {
+	inputFlag := flag.String("input", "", "Input file to load the configuration from, defaults to stdin.")
+	outputFlag := flag.String("output", "", "Output file to use as output, defaults to stdout.")
+	flag.Parse()
+
+	input, err := readInput(*inputFlag)
+	if err != nil {
+		logger.Errorf("Error loading the input: %s", err)
+		os.Exit(inputErrCode)
+	}
+	output, err := configurator.Parse(input)
+	if err != nil {
+		logger.Errorf("Error parsing the configuration: %s", err)
+		os.Exit(parseErrCode)
+	}
+	if err := writeOutput(*outputFlag, output); err != nil {
+		logger.Errorf("Error parsing the configuration: %s", err)
+		os.Exit(outputErrCode)
+	}
 }
