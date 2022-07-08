35b4eb23bae2cd9ec07214324e79f5d31fab8f66
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-07-08 09:22:16 +0200

    chore: update comments

--- configurator/output.go
+++ configurator/output.go
@@ -1,10 +1,12 @@
 package configurator
 
-// Output holds all configuration information in prometheus format which can be directly exported valid prometheus config yaml.
+// Output holds all configuration information in prometheus format which can be directly marshaled to a valid yaml configuration.
 type Output struct {
 	RemoteWrite []interface{} `yaml:"remote_write"`
 }
 
+// BuildOutput builds the prometheus config output from the provided input, it holds "first level" transformations required
+// to obtain a valid prometheus configuration.
 func BuildOutput(input *Input) (Output, error) {
 	output := Output{
 		RemoteWrite: []interface{}{BuildRemoteWriteOutput(input)},
