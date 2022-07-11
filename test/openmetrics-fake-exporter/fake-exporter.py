b6d2b8316099c35f1b0ba40914405ae03b1b9948
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2022-07-11 11:10:57 +0200

    fix from review

--- test/openmetrics-fake-exporter/fake-exporter.py
+++ test/openmetrics-fake-exporter/fake-exporter.py
@@ -1,14 +1,14 @@
-from prometheus_client import start_http_server, Summary, Info, Enum, Histogram, Gauge, Counter
 import random
 import time
+import prometheus_client as prometheus
 
 # Create a metric to track time spent and requests made.
-METRIC_HISTOGRAM = Histogram('fake_openmetrics_histogram', 'Histogram metric name')
-METRIC_SUMMARY = Summary('fake_openmetrics_summary', 'Summary metric name')
-METRIC_GAUGE = Gauge('fake_openmetrics_gauge', 'Gauge metric name')
-METRIC_COUNTER = Counter('fake_openmetrics_counter_total', 'Counter metric name')
-METRIC_INFO = Info('fake_openmetrics', 'Info metric name')
-METRIC_STATESET = Enum('fake_openmetrics_stateset', 'Description of enum',labelnames=['foo'], states=['starting', 'running', 'stopped'])
+METRIC_HISTOGRAM = prometheus.Histogram('fake_openmetrics_histogram', 'Histogram metric name')
+METRIC_SUMMARY = prometheus.Summary('fake_openmetrics_summary', 'Summary metric name')
+METRIC_GAUGE = prometheus.Gauge('fake_openmetrics_gauge', 'Gauge metric name')
+METRIC_COUNTER = prometheus.Counter('fake_openmetrics_counter_total', 'Counter metric name')
+METRIC_INFO = prometheus.Info('fake_openmetrics', 'Info metric name')
+METRIC_STATESET = prometheus.Enum('fake_openmetrics_stateset', 'Description of enum',labelnames=['foo'], states=['starting', 'running', 'stopped'])
 
 # Decorate function with metric.
 @METRIC_HISTOGRAM.time()
@@ -19,7 +19,7 @@ def process_request(t):
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8000)
+    prometheus.start_http_server(8000)
     # Generate some requests.
 
     METRIC_INFO.info({'version': '1.2.3', 'buildhost': 'foo@bar'})
