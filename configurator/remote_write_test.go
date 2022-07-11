41e146b92dba26c0433456b32870c2d095986de5
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-07-11 16:40:56 +0200

    test: refactor remote_write tests

--- configurator/remote_write_test.go
+++ configurator/remote_write_test.go
@@ -5,36 +5,114 @@ package configurator
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRemoteWriteParseProd(t *testing.T) {
+//nolint:funlen
+func TestBuildRemoteWriteOutput(t *testing.T) {
 	t.Parallel()
 
-	prodInput := &Input{
-		RemoteWrite: RemoteWriteInput{
-			Staging:    false,
-			LicenseKey: "fake-prod",
+	cases := []struct {
+		Name     string
+		Input    *Input
+		Expected RemoteWriteOutput
+	}{
+		{
+			Name: "Prod,  non-eu and only mandatory fields",
+			Input: &Input{
+				RemoteWrite: RemoteWriteInput{
+					LicenseKey: "fake-prod",
+				},
+			},
+			Expected: RemoteWriteOutput{
+				URL: "https://metric-api.newrelic.com/prometheus/v1/write",
+				Authorization: Authorization{
+					Credentials: "fake-prod",
+				},
+			},
+		},
+		{
+			Name: "Staging, eu and all fields set",
+			Input: &Input{
+				DataSourceName: "source-of-metrics",
+				RemoteWrite: RemoteWriteInput{
+					LicenseKey: "eu-fake-staging",
+					Staging:    true,
+					ProxyURL:   "http://proxy.url",
+					TLSConfig: &TLSConfig{
+						CAFile:             "ca-file",
+						CertFile:           "cert-file",
+						KeyFile:            "key-file",
+						ServerName:         "server.name",
+						InsecureSkipVerify: true,
+						MinVersion:         "TLS12",
+					},
+					QueueConfig: &QueueConfig{
+						Capacity:          100,
+						MaxShards:         10,
+						MinShards:         2,
+						MaxSamplesPerSend: 1000,
+						BatchSendDeadLine: time.Second,
+						MinBackoff:        100 * time.Microsecond,
+						MaxBackoff:        time.Second,
+						RetryOnHTTP429:    true,
+					},
+					RemoteTimeout: 10 * time.Second,
+					ExtraWriteRelabelConfigs: []PrometheusExtraConfig{
+						map[string]interface{}{
+							"source_labels": []interface{}{"src.label"},
+							"regex":         "to_drop.*",
+							"action":        "drop",
+						},
+					},
+				},
+			},
+			Expected: RemoteWriteOutput{
+				URL:           "https://staging-metric-api.eu.newrelic.com/prometheus/v1/write?prometheus_server=source-of-metrics",
+				RemoteTimeout: 10 * time.Second,
+				Authorization: Authorization{
+					Credentials: "eu-fake-staging",
+				},
+				ProxyURL: "http://proxy.url",
+				TLSConfig: &TLSConfig{
+					CAFile:             "ca-file",
+					CertFile:           "cert-file",
+					KeyFile:            "key-file",
+					ServerName:         "server.name",
+					InsecureSkipVerify: true,
+					MinVersion:         "TLS12",
+				},
+				QueueConfig: &QueueConfig{
+					Capacity:          100,
+					MaxShards:         10,
+					MinShards:         2,
+					MaxSamplesPerSend: 1000,
+					BatchSendDeadLine: time.Second,
+					MinBackoff:        100 * time.Microsecond,
+					MaxBackoff:        time.Second,
+					RetryOnHTTP429:    true,
+				},
+				WriteRelabelConfigs: []PrometheusExtraConfig{
+					map[string]interface{}{
+						"source_labels": []interface{}{"src.label"},
+						"regex":         "to_drop.*",
+						"action":        "drop",
+					},
+				},
+			},
 		},
 	}
-	output := BuildRemoteWriteOutput(prodInput)
-	assert.Equal(t, "https://metric-api.newrelic.com/prometheus/v1/write", output.URL)
-	assert.Equal(t, "fake-prod", output.Authorization.Credentials)
-}
 
-func TestRemoteWriteParseStaging(t *testing.T) {
-	t.Parallel()
-
-	prodInput := &Input{
-		RemoteWrite: RemoteWriteInput{
-			Staging:    true,
-			LicenseKey: "fake-staging",
-		},
+	for _, tc := range cases {
+		c := tc
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+			output := BuildRemoteWriteOutput(c.Input)
+			assert.EqualValues(t, c.Expected, output)
+		})
 	}
-	output := BuildRemoteWriteOutput(prodInput)
-	assert.Equal(t, "https://staging-metric-api.newrelic.com/prometheus/v1/write", output.URL)
-	assert.Equal(t, "fake-staging", output.Authorization.Credentials)
 }
 
 func TestRemoteWriteURL(t *testing.T) {
