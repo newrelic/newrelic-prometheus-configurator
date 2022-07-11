7a79a546b9ab9abcb200debe2e63863f79ae59cf
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-07-11 16:59:40 +0200

    chore: main functon lint

--- cmd/configurator/configurator.go
+++ cmd/configurator/configurator.go
@@ -7,10 +7,10 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"newrelic-prometheus/configurator"
 	"os"
 
-	"newrelic-prometheus/configurator"
-
+	"github.com/scaleway/scaleway-sdk-go/logger"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,9 +20,9 @@ const (
 	parseErrCode
 )
 
-var logger = log.StandardLogger()
-
 func main() {
+	logger := log.StandardLogger()
+
 	inputFlag := flag.String("input", "", "Input file to load the configuration from, defaults to stdin.")
 	outputFlag := flag.String("output", "", "Output file to use as output, defaults to stdout.")
 	flag.Parse()
@@ -32,11 +32,13 @@ func main() {
 		logger.Errorf("Error loading the input: %s", err)
 		os.Exit(inputErrCode)
 	}
+
 	output, err := configurator.Parse(input)
 	if err != nil {
 		logger.Errorf("Error parsing the configuration: %s", err)
 		os.Exit(parseErrCode)
 	}
+
 	err = writeOutput(*outputFlag, output)
 	if err != nil {
 		logger.Errorf("Error writing the output configuration: %s", err)
@@ -50,34 +52,44 @@ func readInput(inputPath string) ([]byte, error) {
 		if err != nil {
 			return nil, fmt.Errorf("could not read from stdin: %w", err)
 		}
+
 		return input, nil
 	}
+
 	fileReader, err := os.Open(inputPath)
 	if err != nil {
 		return nil, fmt.Errorf("the input file could not be opened: %w", err)
 	}
+
 	defer closeLoggingErr(fileReader)
+
 	input, err := io.ReadAll(fileReader)
 	if err != nil {
 		return nil, fmt.Errorf("could not read from the input file: %w", err)
 	}
+
 	return input, nil
 }
 
 func writeOutput(outputPath string, output []byte) error {
 	if outputPath == "" {
 		_, err := os.Stdout.Write(output)
+
 		return fmt.Errorf("could not to stdout: %w", err)
 	}
+
 	fileWriter, err := os.Create(outputPath)
 	if err != nil {
 		return fmt.Errorf("the output file cannot be created: %w", err)
 	}
+
 	defer closeLoggingErr(fileWriter)
+
 	_, err = fileWriter.Write(output)
 	if err != nil {
 		return fmt.Errorf("could not write the output: %w", err)
 	}
+
 	return nil
 }
 
