c3e62fbc4c7e234ef579f9394f10db53ba87b6ac
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2022-07-20 14:37:34 +0200

    test: Add integration tests (#6)

---
+++ test/integration/asserter.go
@@ -0,0 +1,157 @@
+package integration
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/model/exemplar"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/stretchr/testify/require"
+)
+
+var ErrTimeout = errors.New("timeout Exceeded")
+
+type asserter struct {
+	appendable     *mockAppendable
+	defaultTimeout time.Duration
+	defaultBackoff time.Duration
+	prometheusPort string
+}
+
+func newAsserter(prometheusPort string) *asserter {
+	a := &asserter{}
+
+	a.appendable = &mockAppendable{
+		latestSamples: make(map[string]mockSample),
+	}
+
+	a.defaultBackoff = time.Second
+	a.defaultTimeout = time.Second * 10
+	a.prometheusPort = prometheusPort
+
+	return a
+}
+
+func (a *asserter) metricName(t *testing.T, expectedMetricName ...string) {
+	t.Helper()
+
+	var lastNotFound string
+
+	err := retryUntilTrue(a.defaultTimeout, a.defaultBackoff, func() bool {
+		for _, mn := range expectedMetricName {
+			if !a.appendable.HasMetric(mn) {
+				lastNotFound = mn
+				return false
+			}
+		}
+
+		return true
+	})
+	require.NoError(t, err, "metric not found: ", lastNotFound)
+}
+
+func (a *asserter) prometheusServerReady(t *testing.T) {
+	t.Helper()
+
+	err := retryUntilTrue(a.defaultTimeout, a.defaultBackoff, func() bool {
+		resp, err := http.Get(fmt.Sprintf("http://localhost:%s/-/healthy", a.prometheusPort))
+		if err != nil {
+			return false
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return false
+		}
+
+		return true
+	})
+	require.NoError(t, err, "readiness probe failed")
+}
+
+func startRemoteWriteEndpoint(t *testing.T, appendable storage.Appendable) *httptest.Server {
+	t.Helper()
+
+	handler := remote.NewWriteHandler(nil, appendable)
+
+	remoteWriteServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+	}))
+
+	t.Cleanup(func() {
+		remoteWriteServer.Close()
+	})
+
+	return remoteWriteServer
+}
+
+func retryUntilTrue(timeout time.Duration, backoff time.Duration, f func() bool) error {
+	timeoutTicker := time.After(timeout)
+
+	for {
+		if f() {
+			break
+		}
+
+		select {
+		case <-timeoutTicker:
+			return ErrTimeout
+		default:
+			time.Sleep(backoff)
+		}
+	}
+
+	return nil
+}
+
+// mockAppendable implements the github.com/prometheus/prometheus/storage.Appendable interface
+// which is used by the remote write server to store the received samples.
+type mockAppendable struct {
+	latestSamples map[string]mockSample
+	lock          sync.Mutex
+}
+
+type mockSample struct {
+	labels    labels.Labels
+	timestamp int64
+	value     float64
+}
+
+func (m *mockAppendable) Appender(_ context.Context) storage.Appender { //nolint: ireturn // External interface.
+	return m
+}
+
+func (m *mockAppendable) Append(_ storage.SeriesRef, l labels.Labels, t int64, v float64) (storage.SeriesRef, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.latestSamples[l.Get("__name__")] = mockSample{l, t, v}
+
+	return 0, nil
+}
+
+func (m *mockAppendable) Commit() error {
+	return nil
+}
+
+func (*mockAppendable) Rollback() error {
+	return nil
+}
+
+func (m *mockAppendable) AppendExemplar(_ storage.SeriesRef, _ labels.Labels, _ exemplar.Exemplar) (storage.SeriesRef, error) {
+	return 0, nil
+}
+
+func (m *mockAppendable) HasMetric(metricName string) bool {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	_, ok := m.latestSamples[metricName]
+
+	return ok
+}
---
+++ test/integration/integration_test.go
@@ -0,0 +1,102 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	prometheusVersion = "2.36.2"
+)
+
+func TestMain(m *testing.M) {
+	if err := fetchPrometheusBinary(prometheusVersion); err != nil {
+		log.Fatalf("fail to fetch prometheus binary: %s", err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func Test_ServerReady(t *testing.T) {
+	t.Parallel()
+
+	ps := newPrometheusServer(t)
+
+	asserter := newAsserter(ps.port)
+
+	inputConfig := `
+data_source_name: "data-source"
+newrelic_remote_write:
+  license_key: nrLicenseKey
+`
+
+	outputConfigPath := runConfigurator(t, inputConfig)
+
+	ps.start(t, outputConfigPath)
+
+	asserter.prometheusServerReady(t)
+}
+
+func Test_SelfMetrics(t *testing.T) {
+	t.Parallel()
+
+	ps := newPrometheusServer(t)
+
+	asserter := newAsserter(ps.port)
+
+	rw := startRemoteWriteEndpoint(t, asserter.appendable)
+
+	// TODO this test is using a Prom config directly since static targets
+	// are not implemented in the configurator yet.
+	promConfig := path.Join(t.TempDir(), "test-config.yml")
+	err := ioutil.WriteFile(promConfig, []byte(fmt.Sprintf(`
+remote_write:
+- url: http://foo:8999/write
+  proxy_url: %s
+
+global:
+  scrape_interval: 1s
+
+scrape_configs:
+- job_name: self
+  static_configs:
+  - targets: ["localhost:%s"]
+`, rw.URL, ps.port)), 0o444)
+	require.NoError(t, err)
+
+	ps.start(t, promConfig)
+
+	asserter.metricName(t, "prometheus_build_info")
+}
+
+func runConfigurator(t *testing.T, inputConfig string) string {
+	t.Helper()
+
+	tempDir := t.TempDir()
+	inputConfigPath := path.Join(tempDir, "input.yml")
+	outputConfigPath := path.Join(tempDir, "output.yml")
+
+	err := ioutil.WriteFile(inputConfigPath, []byte(inputConfig), 0o444)
+	require.NoError(t, err)
+
+	// nolint:gosec
+	configurator := exec.Command(
+		"go",
+		"run",
+		"../../cmd/configurator",
+		fmt.Sprintf("--input=%s", inputConfigPath),
+		fmt.Sprintf("--output=%s", outputConfigPath),
+	)
+
+	out, err := configurator.CombinedOutput()
+	require.NoError(t, err, string(out))
+
+	return outputConfigPath
+}
