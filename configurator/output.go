1d052f540a68dc1d5af437bb7c0a994bfc64ceca
alvarocabanas <acabanas@newrelic.com>
2022-07-20 17:54:54 +0200

    Add static targets to the output and the parser_test

--- configurator/output.go
+++ configurator/output.go
@@ -7,15 +7,21 @@ package configurator
 // Output holds all configuration information in prometheus format which can be directly marshaled to a valid yaml
 // configuration.
 type Output struct {
-	RemoteWrite []interface{} `yaml:"remote_write"`
+	RemoteWrite   []any `yaml:"remote_write"`
+	ScrapeConfigs []any `yaml:"scrape_configs"`
 }
 
 // BuildOutput builds the prometheus config output from the provided input, it holds "first level" transformations
 // required to obtain a valid prometheus configuration.
 func BuildOutput(input *Input) (Output, error) {
 	output := Output{
-		RemoteWrite: []interface{}{BuildRemoteWriteOutput(input)},
+		RemoteWrite: []any{BuildRemoteWriteOutput(input)},
 	}
+
+	if staticTargets := BuildStaticTargetsOutput(input); len(staticTargets) > 0 {
+		output.ScrapeConfigs = append(output.ScrapeConfigs, staticTargets...)
+	}
+
 	// Include extra remote-write configs
 	for _, extraRemoteWriteConfig := range input.ExtraRemoteWrite {
 		output.RemoteWrite = append(output.RemoteWrite, extraRemoteWriteConfig)
