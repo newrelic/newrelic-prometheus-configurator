752fd2b2b2856b5f1d0f26f357b570d163287b19
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-07-27 09:11:42 +0200

    fix typo

--- configurator/kubernetes_pods.go
+++ configurator/kubernetes_pods.go
@@ -2,7 +2,5 @@ package configurator
 
 // podSettingsBuilder returns a copy of `tg` including the specific settings for when pods kind is set.
 func podSettingsBuilder(tg TargetJobOutput, _ KubernetesJob) TargetJobOutput {
-	tg.KubernetesSdConfigs = []map[string]string{{"role": "pod"}}
-	tg.RelabelConfigs = append(tg.RelabelConfigs)
 	return tg
 }
