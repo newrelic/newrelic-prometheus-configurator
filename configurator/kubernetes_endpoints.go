27e2e015d27894455de96f1d008b61869bc92803
marcsanmi <marcsanmiquel@gmail.com>
2022-08-01 13:02:50 +0200

    fix: remove non-standard prometheus labels

--- configurator/kubernetes_endpoints.go
+++ configurator/kubernetes_endpoints.go
@@ -8,74 +8,52 @@ func endpointSettingsBuilder(job JobOutput, _ KubernetesJob) JobOutput {
 	}
 	job.RelabelConfigs = append(job.RelabelConfigs,
 		RelabelConfig{
-			SourceLabels: []string{"__meta_kubernetes_service_annotation_prometheus_io_scrape"},
-			Action:       "keep",
-			Regex:        "true",
-		},
-		RelabelConfig{
-			SourceLabels: []string{"__meta_kubernetes_service_annotation_prometheus_io_scrape_slow"},
+			SourceLabels: []string{"__meta_kubernetes_pod_phase"},
 			Action:       "drop",
-			Regex:        "true",
+			Regex:        "Pending|Succeeded|Failed|Completed",
 		},
 		RelabelConfig{
 			SourceLabels: []string{"__meta_kubernetes_service_annotation_prometheus_io_scheme"},
 			Action:       "replace",
-			TargetLabel:  "__metrics_path__",
-			Regex:        `(.+)`,
+			TargetLabel:  "__scheme__",
+			Regex:        `(https?)`,
 		},
 		RelabelConfig{
-			SourceLabels: []string{"__meta_kubernetes_pod_annotation_prometheus_io_path"},
+			SourceLabels: []string{"__meta_kubernetes_service_annotation_prometheus_io_path"},
 			Action:       "replace",
-			Regex:        "(.+)",
+			Regex:        `(.+)`,
 			TargetLabel:  "__metrics_path__",
 		},
 		RelabelConfig{
 			SourceLabels: []string{"__address__", "__meta_kubernetes_service_annotation_prometheus_io_port"},
 			Action:       "replace",
 			TargetLabel:  "__address__",
-			Regex:        `([^:]+)(?::\d+)?;(\d+)`,
+			Regex:        `(.+?)(?::\d+)?;(\d+)`,
 			Replacement:  "$1:$2",
 		},
 		RelabelConfig{
-			SourceLabels: []string{"__scheme__", "__address__", "__metrics_path__"},
-			Action:       "replace",
-			TargetLabel:  "scrapedTargetURL",
-			Regex:        `(.+);(.+);(.+)`,
-			Replacement:  "$1://$2$3",
-		},
-		RelabelConfig{
-			SourceLabels: []string{"__meta_kubernetes_endpoints_name"},
-			Action:       "replace",
-			TargetLabel:  "targetName",
-		},
-		RelabelConfig{
-			SourceLabels: []string{"job"},
-			Action:       "replace",
-			TargetLabel:  "scrappedTargetKind",
-		},
-		RelabelConfig{
-			Action: "labeldrop",
-			Regex:  "job",
-		},
-		RelabelConfig{
 			Action:      "labelmap",
-			Regex:       "__meta_kubernetes_pod_label_(.+)",
-			Replacement: "label_$1",
+			Regex:       `__meta_kubernetes_service_annotation_prometheus_io_param_(.+)`,
+			Replacement: "__param_$1",
+		},
+		RelabelConfig{
+			Action: "labelmap",
+			Regex:  `__meta_kubernetes_service_label_(.+)`,
 		},
 		RelabelConfig{
 			SourceLabels: []string{"__meta_kubernetes_namespace"},
 			Action:       "replace",
-			TargetLabel:  "namespaceName",
+			TargetLabel:  "namespace",
 		},
 		RelabelConfig{
 			SourceLabels: []string{"__meta_kubernetes_service_name"},
 			Action:       "replace",
-			TargetLabel:  "serviceName",
+			TargetLabel:  "service",
 		},
 		RelabelConfig{
-			SourceLabels: []string{"__meta_kubernetes_pod_phase"},
-			Action:       "drop",
-			Regex:        "Pending|Succeeded|Failed|Completed",
+			SourceLabels: []string{"__meta_kubernetes_pod_node_name"},
+			Action:       "replace",
+			TargetLabel:  "node",
 		},
 	)
 
