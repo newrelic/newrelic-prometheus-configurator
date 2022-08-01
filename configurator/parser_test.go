41864a6e7dc63cb289987d68aab101ae06a3a598
marcsanmi <marcsanmiquel@gmail.com>
2022-08-01 16:43:07 +0200

    test: skip linter with tparallel

--- configurator/parser_test.go
+++ configurator/parser_test.go
@@ -48,6 +48,7 @@ func TestParser(t *testing.T) {
 	}
 }
 
+//nolint: tparallel
 func TestDataSourceName(t *testing.T) {
 	configWithDataSourceName := `
 data_source_name: %s
@@ -75,6 +76,7 @@ newrelic_remote_write:
 	})
 }
 
+//nolint: tparallel
 func TestLicenseKey(t *testing.T) {
 	configWithLicense := `
 newrelic_remote_write:
