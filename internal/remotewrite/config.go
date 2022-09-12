ad16b3e6ca098774f109f5e3ee4daabf392e9d47
Juan Manuel "Kang" Pérez <kangcoding@gmail.com>
2022-09-12 16:57:35 +0200

    Add FedRAMP support (#73)
    * Add FedRAMP support
    * Add FedRAMP to the chart
    * assert that there are no errors
    * address variable naming nit
    * golangci-lint pickiness
    * change testing convention to a black box test
    * rename all URL variables

--- internal/remotewrite/config.go
+++ internal/remotewrite/config.go
@@ -4,24 +4,16 @@
 package remotewrite
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/newrelic/newrelic-prometheus-configurator/internal/promcfg"
-)
-
-const (
-	remoteWriteBaseURL       = "https://%smetric-api.%snewrelic.com/prometheus/v1/write"
-	environmentStagingPrefix = "staging-"
-	regionEUPrefix           = "eu."
-	// prometheusServerQueryParam is added to remoteWrite url when nrConfig's name is defined.
-	prometheusServerQueryParam = "prometheus_server"
 )
 
 // Config defines all the NewRelic's remote write endpoint fields.
 type Config struct {
 	LicenseKey               string                  `yaml:"license_key"`
 	Staging                  bool                    `yaml:"staging"`
+	FedRAMP                  FedRAMP                 `yaml:"fedramp"`
 	ProxyURL                 string                  `yaml:"proxy_url"`
 	TLSConfig                *promcfg.TLSConfig      `yaml:"tls_config"`
 	QueueConfig              *promcfg.QueueConfig    `yaml:"queue_config"`
@@ -29,10 +21,28 @@ type Config struct {
 	ExtraWriteRelabelConfigs []promcfg.RelabelConfig `yaml:"extra_write_relabel_configs"`
 }
 
+// FedRAMP in charts are configured like `.fedramp.enabled: true` just in case we have to
+// add more options to fedramp dictionary. So we add a strict for it.
+type FedRAMP struct {
+	Enabled bool `yaml:"enabled"`
+}
+
 // Build will create the Prometheus remote_write entry for NewRelic.
-func (c Config) Build(dataSourceName string) promcfg.RemoteWrite {
-	return promcfg.RemoteWrite{
-		URL:                 remoteWriteURL(c.Staging, c.LicenseKey, dataSourceName),
+func (c Config) Build(dataSourceName string) (promcfg.RemoteWrite, error) {
+	rwu := NewURL(
+		WithFedRAMP(c.FedRAMP.Enabled),
+		WithLicense(c.LicenseKey),
+		WithStaging(c.Staging),
+		WithDataSourceName(dataSourceName),
+	)
+
+	url, err := rwu.Build()
+	if err != nil {
+		return promcfg.RemoteWrite{}, err
+	}
+
+	rw := promcfg.RemoteWrite{
+		URL:                 url,
 		RemoteTimeout:       c.RemoteTimeout,
 		Authorization:       promcfg.Authorization{Credentials: c.LicenseKey},
 		TLSConfig:           c.TLSConfig,
@@ -40,22 +50,6 @@ func (c Config) Build(dataSourceName string) promcfg.RemoteWrite {
 		QueueConfig:         c.QueueConfig,
 		WriteRelabelConfigs: c.ExtraWriteRelabelConfigs,
 	}
-}
 
-func remoteWriteURL(staging bool, licenseKey string, dataSourceName string) string {
-	envPrefix, regionPrefix := "", ""
-	if licenseIsRegionEU(licenseKey) {
-		regionPrefix = regionEUPrefix
-	}
-
-	if staging {
-		envPrefix = environmentStagingPrefix
-	}
-
-	url := fmt.Sprintf(remoteWriteBaseURL, envPrefix, regionPrefix)
-	if dataSourceName != "" {
-		url = url + "?" + prometheusServerQueryParam + "=" + dataSourceName
-	}
-
-	return url
+	return rw, nil
 }
---
+++ internal/remotewrite/remote_write_url_test.go
@@ -0,0 +1,124 @@
+package remotewrite_test
+
+import (
+	"testing"
+
+	"github.com/newrelic/newrelic-prometheus-configurator/internal/remotewrite"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoteWriteURL(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		Name           string
+		Staging        bool
+		FedRAMP        bool
+		LicenseKey     string
+		Expected       string
+		DataSourceName string
+	}{
+		{
+			Name:       "staging non-eu",
+			Staging:    true,
+			FedRAMP:    false,
+			LicenseKey: "non-eu-license-key",
+			Expected:   "https://staging-metric-api.newrelic.com/prometheus/v1/write",
+		},
+		{
+			Name:       "staging eu",
+			Staging:    true,
+			FedRAMP:    false,
+			LicenseKey: "eu-license-key",
+			Expected:   "https://staging-metric-api.eu.newrelic.com/prometheus/v1/write",
+		},
+		{
+			Name:       "prod non-eu",
+			Staging:    false,
+			FedRAMP:    false,
+			LicenseKey: "non-eu-license-key",
+			Expected:   "https://metric-api.newrelic.com/prometheus/v1/write",
+		},
+		{
+			Name:       "prod -eu",
+			Staging:    false,
+			FedRAMP:    false,
+			LicenseKey: "eu-license-key",
+			Expected:   "https://metric-api.eu.newrelic.com/prometheus/v1/write",
+		},
+		{
+			Name:       "fedramp",
+			Staging:    false,
+			FedRAMP:    true,
+			LicenseKey: "non-eu-license-key",
+			Expected:   "https://gov-metric-api.newrelic.com/prometheus/v1/write",
+		},
+		{
+			Name:           "dataSourceName",
+			Staging:        false,
+			FedRAMP:        false,
+			LicenseKey:     "non-eu-license-key",
+			Expected:       "https://metric-api.newrelic.com/prometheus/v1/write?prometheus_server=source",
+			DataSourceName: "source",
+		},
+	}
+
+	for _, testCase := range cases {
+		c := testCase
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+			rwu := remotewrite.NewURL(
+				remotewrite.WithFedRAMP(c.FedRAMP),
+				remotewrite.WithLicense(c.LicenseKey),
+				remotewrite.WithStaging(c.Staging),
+				remotewrite.WithDataSourceName(c.DataSourceName),
+			)
+			result, err := rwu.Build()
+			if assert.NoError(t, err) {
+				assert.Equal(t, c.Expected, result)
+			}
+		})
+	}
+}
+
+func TestRemoteWriteURLErrors(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		Name           string
+		Staging        bool
+		FedRAMP        bool
+		LicenseKey     string
+		Expected       error
+		DataSourceName string
+	}{
+		{
+			Name:     "staging FedRAMP",
+			Staging:  true,
+			FedRAMP:  true,
+			Expected: remotewrite.ErrFedRAMPStaging,
+		},
+		{
+			Name:       "European FedRAMP",
+			Staging:    false,
+			FedRAMP:    true,
+			LicenseKey: "eu-license-key",
+			Expected:   remotewrite.ErrEuFedRAMP,
+		},
+	}
+
+	for _, testCase := range cases {
+		c := testCase
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+			rwu := remotewrite.NewURL(
+				remotewrite.WithFedRAMP(c.FedRAMP),
+				remotewrite.WithLicense(c.LicenseKey),
+				remotewrite.WithStaging(c.Staging),
+				remotewrite.WithDataSourceName(c.DataSourceName),
+			)
+			_, err := rwu.Build()
+			assert.Equal(t, c.Expected, err)
+		})
+	}
+}
