56361c611c5b1f808d3e7f95970a506897d8b6b8
Diogo Pacheco <100862257+dpacheconr@users.noreply.github.com>
2025-04-09 11:48:16 +0100

    support configuring proxy via environment variables

--- internal/configurator/nr_config_test.go
+++ internal/configurator/nr_config_test.go
@@ -17,10 +17,15 @@ import (
 func TestNrConfig(t *testing.T) {
 	t.Parallel()
 
-	expected := testNrConfigExpectation(t)
+	expected := testNrConfigExpectation(t, false)
 	nrConfigData, err := os.ReadFile("testdata/nr-config-test.yaml")
 	require.NoError(t, err)
 
+	checkNrConfig(t, expected, nrConfigData)
+
+	expected = testNrConfigExpectation(t, true)
+	nrConfigData, err = os.ReadFile("testdata/nr-config-test-proxyfromenv.yaml")
+	require.NoError(t, err)
 	checkNrConfig(t, expected, nrConfigData)
 }
 
@@ -33,11 +38,50 @@ func checkNrConfig(t *testing.T, expected NrConfig, nrConfigData []byte) {
 	require.EqualValues(t, expected, nrConfig)
 }
 
-func testNrConfigExpectation(t *testing.T) NrConfig {
+func testNrConfigExpectation(t *testing.T, useProxyFromEnv bool) NrConfig {
 	t.Helper()
 
 	trueValue := true
 	falseValue := false
+
+	remoteWriteConfig := remotewrite.Config{
+		DataSourceName: "data-source",
+		LicenseKey:     "nrLicenseKey",
+		Staging:        true,
+		TLSConfig: &promcfg.TLSConfig{
+			InsecureSkipVerify: &trueValue,
+			CAFile:             "/path/to/ca.crt",
+			CertFile:           "/path/to/cert.crt",
+			KeyFile:            "/path/to/key.crt",
+			ServerName:         "server.name",
+			MinVersion:         "TLS12",
+		},
+		QueueConfig: &promcfg.QueueConfig{
+			Capacity:          2500,
+			MaxShards:         200,
+			MinShards:         1,
+			MaxSamplesPerSend: 500,
+			BatchSendDeadLine: 5 * time.Second,
+			MinBackoff:        30 * time.Millisecond,
+			MaxBackoff:        5 * time.Second,
+			RetryOnHTTP429:    &falseValue,
+		},
+		RemoteTimeout: 30 * time.Second,
+		ExtraWriteRelabelConfigs: []promcfg.RelabelConfig{
+			{
+				SourceLabels: []string{"__name__", "instance"},
+				Regex:        "node_memory_active_bytes;localhost:9100",
+				Action:       "drop",
+			},
+		},
+	}
+
+	// Decide whether to use ProxyURL or ProxyFromEnvironment
+	if useProxyFromEnv {
+		remoteWriteConfig.ProxyFromEnvironment = true
+	} else {
+		remoteWriteConfig.ProxyURL = "http://proxy.url.to.use:1234"
+	}
 
 	return NrConfig{
 		Common: promcfg.GlobalConfig{
@@ -48,38 +92,7 @@ func testNrConfigExpectation(t *testing.T) NrConfig {
 				"three": "four",
 			},
 		},
-		RemoteWrite: remotewrite.Config{
-			DataSourceName: "data-source",
-			LicenseKey:     "nrLicenseKey",
-			Staging:        true,
-			ProxyURL:       "http://proxy.url.to.use:1234",
-			TLSConfig: &promcfg.TLSConfig{
-				InsecureSkipVerify: &trueValue,
-				CAFile:             "/path/to/ca.crt",
-				CertFile:           "/path/to/cert.crt",
-				KeyFile:            "/path/to/key.crt",
-				ServerName:         "server.name",
-				MinVersion:         "TLS12",
-			},
-			QueueConfig: &promcfg.QueueConfig{
-				Capacity:          2500,
-				MaxShards:         200,
-				MinShards:         1,
-				MaxSamplesPerSend: 500,
-				BatchSendDeadLine: 5 * time.Second,
-				MinBackoff:        30 * time.Millisecond,
-				MaxBackoff:        5 * time.Second,
-				RetryOnHTTP429:    &falseValue,
-			},
-			RemoteTimeout: 30 * time.Second,
-			ExtraWriteRelabelConfigs: []promcfg.RelabelConfig{
-				{
-					SourceLabels: []string{"__name__", "instance"},
-					Regex:        "node_memory_active_bytes;localhost:9100",
-					Action:       "drop",
-				},
-			},
-		},
+		RemoteWrite: remoteWriteConfig,
 		ExtraRemoteWrite: []RawPromConfig{
 			map[string]any{
 				"url": "https://extra.prometheus.remote.write",
